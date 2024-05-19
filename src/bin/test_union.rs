99e4877ed407488ef7a4efd0e8d97517bedd4adf
Thomas Draper <tgdrape@gmail.com>
2024-05-19 07:50:09 -0700

    Add rotate_iter, translate_iter and sum() capability.

---
+++ src/bin/test_union.rs
@@ -0,0 +1,20 @@
+// extern crate itertools;
+
+// use std::fmt;
+// use itertools::Itertools;
+// use std::ops::Add;
+use std::cell::RefCell;
+
+use tgdscad::*;
+// use crate::*;
+
+fn main() {
+    let e = D2::Circle(4.);
+    // let f = e.translate_iter(1.,2.,10) .collect::<Vec<_>>() ;
+    // let g = D2::Union(RefCell::new(f));
+    let g = e.translate_iter(1.,2.,10).sum::<D2>();
+
+    // println!("{:?}", &f);
+    println!("{}", &g);
+}
+
--- src/scad2d.rs
+++ src/scad2d.rs
@@ -2,9 +2,11 @@ extern crate itertools;
 
 use std::fmt;
 use itertools::Itertools;
+use std::iter::Sum;
 // use std::ops::Add;
 use std::cell::RefCell;
 
+// pub struct D2_iter(Vec<D2>);
 
 #[derive(Clone, Debug)]
 pub enum D2 {
@@ -18,6 +20,7 @@ pub enum D2 {
     Scale(f32, Box<D2>),
     ScaleXY((f32, f32), Box<D2>),
     Translate((f32, f32), Box<D2>),
+    Rotate(f32, Box<D2>),
 }
 
 pub fn indent(shape: &D2) -> String {
@@ -30,19 +33,51 @@ impl D2 {
         // D2::Union(Box::new(vec![self, other]))
         D2::Union(RefCell::new(vec![self, other]))
     }
+
     pub fn minkowski(self, other: D2) -> D2 {
         D2::Minkowski(Box::new(self), Box::new(other))
     }
-    pub fn translate(self, x: f32, y: f32) -> D2 {
-        D2::Translate((x,y), Box::new(self))
+
+    pub fn translate(&self, x: f32, y: f32) -> D2 {
+        D2::Translate((x,y), Box::new(self.clone()))
     }
+
+    pub fn translate_iter<'a>(&'a self, x: f32, y: f32, n: u32) -> impl Iterator<Item = D2> + 'a {
+        (0..n).map(move |ii| self.translate(x * ii as f32, y * ii as f32))
+    }
+
+    pub fn rotate(&self, theta: f32) -> D2 {
+        D2::Rotate(theta, Box::new(self.clone()))
+    }
+
+    pub fn rotate_iter<'a>(&'a self, theta: f32, n: u32) -> impl Iterator<Item = D2> + 'a {
+        (0..n).map(move |ii| self.rotate(theta * ii as f32))
+    }
+
     pub fn scale(self, s: f32) -> D2 {
         D2::Scale(s, Box::new(self))
     }
+
     pub fn scale_xy(self, x: f32, y: f32) -> D2 {
         D2::ScaleXY((x,y), Box::new(self))
     }
 }
+
+impl Sum for D2 {
+    fn sum<I>(iter: I) -> Self
+      where 
+        I: Iterator<Item = Self>
+    {
+        D2::Union(RefCell::new(iter.collect::<Vec<Self>>()))
+    }
+}
+
+// impl D2_iter {
+    // pub fn union(&self) -> D2 {
+        // D2::Union(RefCell::new(self.collect::<Vec<_>>()))
+    // }
+// }
+
 
 impl fmt::Display for D2 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -52,6 +87,8 @@ impl fmt::Display for D2 {
             D2::Rectangle(x, y) => write!(f, "square(size = [{}, {}]);", x, y),
             D2::Translate((x,y), shape) => write!(f, 
                 "translate(v = [{}, {}]) {{\n  {}\n}}", x,y, indent(shape)),
+            D2::Rotate(theta, shape) => write!(f, 
+                "rotate({}) {{\n  {}\n}}", theta, indent(shape)),
             D2::Scale(s, shape) => write!(f,
                 "scale(v = {}) {{\n  {}\n}}", s, indent(shape)),
             D2::ScaleXY((x,y), shape) => write!(f,
@@ -61,7 +98,7 @@ impl fmt::Display for D2 {
             // D2::Union(v) => write!(f,
                 // "union() {{\n  {}\n  {}\n}}", indent(&v[0]), indent(&v[1])),
             D2::Union(v) => write!(f,
-                "union() {{\n  {}\n}}", v.borrow().iter().map(|x| format!("{}", x)).collect::<Vec<_>>().join("\n")),
+                "union() {{\n  {}\n}}", v.borrow().iter().map(|x| format!("{}", indent(x))).collect::<Vec<_>>().join("\n  ")),
             D2::Minkowski(a,b) => write!(f,
                 "minkowski() {{\n  {}\n  {}\n}}", indent(a), indent(b)),
         }
@@ -71,19 +108,47 @@ impl fmt::Display for D2 {
 #[cfg(test)]
 mod test {
     use super::*;
+    use lazy_static::lazy_static;
+
+    // lazy_static! {
+        // static ref c5: D2 = D2::Circle(5.);
+    // }
+    const c5: D2 = D2::Circle(5.);
+    const s9: D2 = D2::Square(9.);
 
     #[test]
     fn test_circle() {
-        assert_eq!(format!("{}", D2::Circle(5.)), "circle(r = 5);");
+        assert_eq!(format!("{}", c5), "circle(r = 5);");
     }
 
     #[test]
     fn test_square() {
-        assert_eq!(format!("{}", D2::Square(5.)), "square(size = 5);");
+        assert_eq!(format!("{}", s9), "square(size = 9);");
+    }
+
+    #[test]
+    fn test_add() {
+        assert_eq!(format!("{}", c5.add(s9)),
+        "union() {\n  circle(r = 5);\n  square(size = 9);\n}");
     }
 
     #[test]
     fn test_union() {
-        assert_eq!(format!("{}", D2::Circle(5.).add(D2::Square(10.))), "circle(r = 5);");
+        assert_eq!(format!("{}", c5.add(s9)),
+        "union() {\n  circle(r = 5);\n  square(size = 9);\n}");
+    }
+
+    #[test]
+    fn test_translate_iter() {
+        assert_eq!(format!("{}", c5.translate_iter(1.,2.,4).sum::<D2>()),
+            "union() {\n  translate(v = [0, 0]) {\n    circle(r = 5);\n  }\n  translate(v = [1, 2]) {\n    circle(r = 5);\n  }\n  translate(v = [2, 4]) {\n    circle(r = 5);\n  }\n  translate(v = [3, 6]) {\n    circle(r = 5);\n  }\n}"
+        );
+    }
+
+    #[test]
+    fn test_rotate_iter() {
+        assert_eq!(format!("{}", s9.rotate_iter(20., 4).sum::<D2>()),
+            "union() {\n  rotate(0) {\n    square(size = 9);\n  }\n  rotate(20) {\n    square(size = 9);\n  }\n  rotate(40) {\n    square(size = 9);\n  }\n  rotate(60) {\n    square(size = 9);\n  }\n}"
+        );
     }
 }
