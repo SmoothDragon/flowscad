7ad3890190d0c518b33549d4b98e1d245dbaba1f
Thomas Draper <tgdrape@gmail.com>
2024-07-22 08:24:57 -0700

    Add new bubble cube design

---
+++ src/bin/bubble_cube-scad.rs
@@ -0,0 +1,36 @@
+use flowscad::*;
+
+fn main() {
+    let theta = (-1.0_f64/3.).acos()*180.0/PI;
+    let phi = 90. - (theta/2.);
+    let l_edge = 50.;
+    let dot = D3::truncated_octahedron(0.4);
+    let center = dot.clone()
+        .translate(v3(l_edge * 0.707 * (1. + phi.cos()), 0, 0))
+        .iter_rotate(v3(0,0,90), 4)
+        .hull()
+        ;
+    let wing = dot.clone()
+        .translate(v3(l_edge * 0.707 * (1. + phi.cos()), 0, 0))
+        .add(dot.clone().translate(v3(l_edge * 0.707, 0., l_edge / 2.)))
+        .add(dot.clone().translate(v3(l_edge * 0.707, 0., -l_edge / 2.)))
+        .hull()
+        .iter_rotate(v3(0,0,90), 4)
+        .union()
+        ;
+    let plate = dot.clone()
+        .translate(v3(l_edge * 0.707 * (1. + phi.cos()), 0, 0))
+        .add(dot.clone().translate(v3(l_edge * 0.707, 0., l_edge / 2.)))
+        .add_map(move |x| x.rotate(v3(0,0,90)))
+        .hull()
+        .iter_rotate(v3(0,0,90), 4)
+        .union()
+        .add_map(move |x| x.rotate(v3(180,0,0)))
+        ;
+    let shape = (wing+plate+center)
+        .rotate(v3(90,45,0))
+        ;
+    
+    println!("{}", &shape);
+}
+
